dc5a52360d2a95e6ac3ba322ead974f1e321a583
Ta-ma <santiago.tamashiro@gmail.com>
2020-04-26 17:36:41 -0300

    Quinto intento de arreglar el format

--- tabla_simbolos.h
+++ tabla_simbolos.h
@@ -4,7 +4,7 @@
 #include <conio.h>
 #include <stdio.h>
 
-#define LIM_SIMBOLOS 	100 // Cantidad de símbolos que va a poder manejar la tabla
+#define LIM_SIMBOLOS     100 // Cantidad de símbolos que va a poder manejar la tabla
 
 typedef struct 
 {   char nombre[100];
@@ -19,60 +19,60 @@ void asignarTipo(int);
 void insertar_ID_en_Tabla(char*);
 int buscarEnTOSID(char*);
 
-int TOStop = 0;	
+int TOStop = 0;    
 tablaDeSimbolos TOS[LIM_SIMBOLOS];
 
 void guardarTOS()
 {
     int i;
-	char aux[100];
-	FILE *tos;         
-	if ((tos = fopen ("TablaDeSimbolos.txt","w"))== NULL)
-	  {
-		  printf("No se puede crear el archivo de la tabla de simbolos");
-		  exit(1);
-	  }
+    char aux[100];
+    FILE *tos;         
+    if ((tos = fopen ("TablaDeSimbolos.txt","w"))== NULL)
+    {
+        printf("No se puede crear el archivo de la tabla de simbolos");
+        exit(1);
+    }
     fprintf(tos,"\n------------------------------ TABLA DE  SIMBOLOS ------------------------------\n");
     for (i=0; i< TOStop; i++)
     {
-		sprintf(aux, "%d", TOS[i].longitud);
-		if(strcmp(aux, "0") == 0)
-			aux[0] = '\0';
+        sprintf(aux, "%d", TOS[i].longitud);
+        if(strcmp(aux, "0") == 0)
+            aux[0] = '\0';
         fprintf(tos,"Nombre: %s  | Tipo: %s   | Valor: %s | Limite: %d | Longitud: %s \n",TOS[i].nombre, TOS[i].tipo, TOS[i].valor, TOS[i].limite, aux);
     }
 
     fprintf(tos,"\n------------------------------ TABLA DE  SIMBOLOS ------------------------------\n");
-	
-	if(fclose(tos)!=0)
-	  {
-		  printf("No se puede CERRAR el archivo de la tabla de simbolos");
-		  exit(1);
-	  }
+    
+    if(fclose(tos)!=0)
+    {
+        printf("No se puede CERRAR el archivo de la tabla de simbolos");
+        exit(1);
+    }
 }
 
 void insertar_ID_en_Tabla(char* token)
 {
-	char aux[100];
-	strcpy(aux,"_@");
-	strcat(aux, token);
-	if(!buscarEnTOSID(aux))
-	{
-		strcpy(TOS[TOStop].nombre, aux);
-		strcpy(TOS[TOStop].tipo,"ID" );
-		TOStop++;
-	}
+    char aux[100];
+    strcpy(aux,"_@");
+    strcat(aux, token);
+    if(!buscarEnTOSID(aux))
+    {
+        strcpy(TOS[TOStop].nombre, aux);
+        strcpy(TOS[TOStop].tipo,"ID" );
+        TOStop++;
+    }
 }
 
 int buscarEnTOSID(char* val)
 {
-	int i;
-	for (i=0; i<TOStop; i++)
+    int i;
+    for (i=0; i<TOStop; i++)
     {
-		if(strcmp(TOS[i].nombre, val) == 0)
-		{
-			return 1;
-		}
-	}
-	
-	return 0;
+        if(strcmp(TOS[i].nombre, val) == 0)
+        {
+            return 1;
+        }
+    }
+    
+    return 0;
 }
